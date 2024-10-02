64db6219fcd7f11823caf858c7f746df3160c853
nearbyapapermacbook <Wuttipat_tua@truecorp.co.th>
2024-10-02 20:56:41 +0700

    The Important of The name of 'main'

---
+++ app.go
@@ -0,0 +1,7 @@
+package main // The Importanf of The name main <-- "main" is tell the go project this is starter of application
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nearbyapaper/go_101
+
+go 1.23.2
