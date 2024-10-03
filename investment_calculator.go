72138f5766163ba15a7e8184ec230820bad24e0c
nearbyapapermacbook <Wuttipat_tua@truecorp.co.th>
2024-10-03 12:32:39 +0700

    short way to declare variable use ':=' operator GO will assign type that should be to the variable

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,14 +6,28 @@ import (
 )
 
 func main() {
-	// fmt.Println("Hello world!")
-	var investmentAmount = 30000
-	var expectedReturnenRate = 5.5
-	var investmentYears = 5
+	// var investmentAmount = 30000
+	// var expectedReturnenRate = 5.5
+	// var investmentYears = 5
 
-	var futureValue = float64(investmentAmount) * math.Pow((1+expectedReturnenRate/100), float64(investmentYears))
+	// short way to declare variable use ':=' operator GO will assign type that should be to the variable
+	investmentAmount := 30000.0
+	expectedReturnenRate := 5.5
+	investmentYears := 5.0
+
+	// calculate future value
+	// math.Pow(base, exponent) returns base raised to exponent
+	// math.Pow(1+expectedReturnenRate/100, float64(investmentYears)) calculates the future value for each year
+	// float64(investmentAmount) converts the investment amount to float64 for the calculation
+	// * is used to perform multiplication in Go, not ^ for exponentiation
+	// float64 is necessary to perform mathematical operations on numbers of different types (int and float64) in Go
+	// float64(investmentAmount) converts investmentAmount to float64 for the calculation
+	// * is used to perform multiplication in Go, not ^ for exponentiation
+	// float64 is necessary to perform mathematical operations
+
+	var futureValue = investmentAmount * math.Pow((1+expectedReturnenRate/100), investmentYears)
 	// convert investmentAmount to float64 by float64(number: int)
 	// can't mix between different type for operation
 
-	fmt.Printf("The future value of investment after %d years is: $%.2f\n", investmentYears, futureValue)
+	fmt.Printf("The future value of investment after %.2f years is: $%.2f\n", investmentYears, futureValue)
 }
