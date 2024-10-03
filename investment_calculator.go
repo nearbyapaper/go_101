c5a79aac7dc62a32662bcaea1ef4676480a432a2
nearbyapapermacbook <Wuttipat_tua@truecorp.co.th>
2024-10-03 12:50:59 +0700

    learning user input

--- investment_calculator.go
+++ investment_calculator.go
@@ -15,6 +15,8 @@ func main() {
 	expectedReturnenRate := 5.5
 	investmentYears := 5.0
 
+	const inflationRate = 3.5 // const is Immutable (can't reassigned)
+
 	// calculate future value
 	// math.Pow(base, exponent) returns base raised to exponent
 	// math.Pow(1+expectedReturnenRate/100, float64(investmentYears)) calculates the future value for each year
@@ -25,9 +27,17 @@ func main() {
 	// * is used to perform multiplication in Go, not ^ for exponentiation
 	// float64 is necessary to perform mathematical operations
 
+	fmt.Println("What's your investment amount : ")
+	fmt.Scan(&investmentAmount) // '&' is pointer operation to the variable
+
+	fmt.Println("What's your expectedReturnenRate : ")
+	fmt.Scan(&expectedReturnenRate) // '&' is pointer operation to the variable
+
 	var futureValue = investmentAmount * math.Pow((1+expectedReturnenRate/100), investmentYears)
 	// convert investmentAmount to float64 by float64(number: int)
 	// can't mix between different type for operation
+	var realFutureValue = futureValue / math.Pow((1+inflationRate/100), investmentYears)
 
 	fmt.Printf("The future value of investment after %.2f years is: $%.2f\n", investmentYears, futureValue)
+	fmt.Println("Real future value of investment : ", realFutureValue)
 }
