c9f00284c91f83e32cb67705227f6186e8ee14ee
nearbyapapermacbook <Wuttipat_tua@truecorp.co.th>
2024-10-06 23:16:54 +0700

    use methods work with struct

--- demo/structs.go
+++ demo/structs.go
@@ -14,6 +14,22 @@ type user struct { // struct blueprints
 	createDate time.Time
 }
 
+func (data user) printUserDetails() {
+	// (data user) calles 'receiver argument'
+	// which is not specified here in regular parameter list
+	// but which(receiver argument) instead is specified here
+	// in frontof the functionname to, as mentioned above
+	fmt.Printf("\n%s %s %d years old\n birthday %s\n email : %s\n this information update when %s\n", data.firstName, data.lastName, data.age, data.birthDay, data.email, data.createDate)
+}
+func (data *user) printUserDetailsByPointer() {
+	// in GO Struct have improved you do not have to dereference for access data pointer in struct
+	fmt.Println("====================================================")
+	fmt.Printf("\n%s %s %d years old\n birthday %s\n email : %s\n this information update when %s\n", data.firstName, data.lastName, data.age, data.birthDay, data.email, data.createDate)
+	// but in real process you have to do like this
+	fmt.Println("====================================================")
+	fmt.Printf("\n%s %s %d years old\n birthday %s\n email : %s\n this information update when %s\n", (*data).firstName, (*data).lastName, (*data).age, (*data).birthDay, (*data).email, (*data).createDate)
+}
+
 func Struct101() {
 	// Create a new user
 	user1 := user{
@@ -27,7 +43,8 @@ func Struct101() {
 
 	fmt.Println("================================This is Struct101================================")
 	// Print the user details
-	printUserDetails(user1)
+	// printUserDetails(user1)
+	user1.printUserDetails()
 
 	fmt.Println("\nUpdated user")
 	// Update the user's email
@@ -36,18 +53,15 @@ func Struct101() {
 
 	// Print the updated user details
 	fmt.Println("================================Use StructPointer Pass to Function================================")
-	printUserDetailsByPointer(&user1)
+	// printUserDetailsByPointer(&user1)
+	user1.printUserDetailsByPointer()
 }
 
-func printUserDetails(data user) {
-	fmt.Printf("\n%s %s %d years old\n birthday %s\n email : %s\n this information update when %s\n", data.firstName, data.lastName, data.age, data.birthDay, data.email, data.createDate)
-}
-
-func printUserDetailsByPointer(data *user) {
-	// in GO Struct have improved you do not have to dereference for access data pointer in struct
-	fmt.Println("====================================================")
-	fmt.Printf("\n%s %s %d years old\n birthday %s\n email : %s\n this information update when %s\n", data.firstName, data.lastName, data.age, data.birthDay, data.email, data.createDate)
-	// but in real process you have to do like this
-	fmt.Println("====================================================")
-	fmt.Printf("\n%s %s %d years old\n birthday %s\n email : %s\n this information update when %s\n", (*data).firstName, (*data).lastName, (*data).age, (*data).birthDay, (*data).email, (*data).createDate)
-}
+// func printUserDetailsByPointer(data *user) {
+// 	// in GO Struct have improved you do not have to dereference for access data pointer in struct
+// 	fmt.Println("====================================================")
+// 	fmt.Printf("\n%s %s %d years old\n birthday %s\n email : %s\n this information update when %s\n", data.firstName, data.lastName, data.age, data.birthDay, data.email, data.createDate)
+// 	// but in real process you have to do like this
+// 	fmt.Println("====================================================")
+// 	fmt.Printf("\n%s %s %d years old\n birthday %s\n email : %s\n this information update when %s\n", (*data).firstName, (*data).lastName, (*data).age, (*data).birthDay, (*data).email, (*data).createDate)
+// }
